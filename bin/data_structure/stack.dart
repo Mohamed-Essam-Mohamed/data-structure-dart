3741487416d31b8aeb927dbed02cd501fdcf9ce7
mohamed <esammww.com@gmail.com>
2024-09-14 15:42:05 +0300

    data structure stack by dart

---
+++ bin/data_structure/stack.dart
@@ -0,0 +1,20 @@
+class Stack<T> {
+  Stack() : _storage = <T>[];
+  final List<T> _storage;
+  @override
+  String toString() {
+    return '--- Top ---\n'
+        '${_storage.reversed.join('\n')}'
+        '\n-----------';
+  }
+
+  bool isEmpty() => _storage.isEmpty;
+  void push(T element) => _storage.add(element);
+  T pop() {
+    if (isEmpty()) {
+      return throw ("is Empty stack can't pop");
+    } else {
+      return _storage.removeLast();
+    }
+  }
+}
---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:data_structure/data_structure.dart' as data_structure;
+
+void main(List<String> arguments) {
+  print('Hello world: ${data_structure.calculate()}!');
+}

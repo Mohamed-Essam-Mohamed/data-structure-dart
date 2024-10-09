d05ca731fa447dd06f2bb53dc39cc03cb0242caf
mohamed <esammww.com@gmail.com>
2024-10-09 23:56:46 +0300

    singly linked list.

---
+++ bin/data_structure/linked_list/singly_linked_list.dart
@@ -0,0 +1,62 @@
+class Node<T> {
+  T data;
+  Node<T>? next;
+
+  Node(this.data, this.next);
+}
+
+class SinglyLinkedList<T> {
+  Node<T>? head;
+  late int length;
+
+  SinglyLinkedList() {
+    head = null;
+    length = 0;
+  }
+  bool isEmpty() => head == null;
+  bool isNotEmpty() => !isEmpty();
+  void add(T element) {
+    Node<T> newElement = Node(element, null);
+    if (isEmpty()) {
+      head = newElement;
+    } else {
+      Node<T> current = head!;
+      while (current.next != null) {
+        current = current.next!;
+      }
+      current.next = newElement;
+    }
+  }
+
+  void remove(T data) {
+    if (head == null) {
+      return;
+    }
+
+    if (head?.data == data) {
+      head = head?.next;
+      return;
+    }
+
+    Node<T>? current = head;
+    while (current?.next != null) {
+      if (current?.next?.data == data) {
+        current?.next = current.next?.next;
+        return;
+      }
+      current = current?.next;
+    }
+  }
+
+  void printLinkedList() {
+    if (isEmpty()) {
+      print("the Linked List is Empty");
+    } else {
+      Node<T>? current = head!;
+      while (current != null) {
+        print(current.data);
+        current = current.next;
+      }
+    }
+  }
+}
